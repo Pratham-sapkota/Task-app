db06997f9bab830123fd1c8fb82409318c32e87d
Pratham Sapkota <prathamsapkota1837@gmail.com>
2022-05-23 10:22:31 +0545

    reading endpoints

--- src/index.js
+++ src/index.js
@@ -18,6 +18,30 @@ app.post('/users',(req,res)=>{
     })
 })
 
+
+app.get('/users',(req,res)=>{
+    User.find({}).then(users=>{
+        res.send(users)
+    }).catch(e=>{
+        res.status(500).send()
+    })
+})
+
+
+app.get('/users/:id',(req,res)=>{
+    const _id=req.params.id
+
+    User.findById(_id).then(user=>{
+        if(!user){
+           return res.status(404).send()
+        }
+        res.send(user)
+    }).catch(e=>{
+            res.status(500).send(e)
+    })
+})
+
+
 app.post('/task',(req,res)=>{
     const task= new Tasks(req.body)
     task.save().then(()=>{
@@ -26,6 +50,19 @@ app.post('/task',(req,res)=>{
         res.status(400).send(e)
     })
 })
+
+app.get('/task/:id',(req,res)=>{
+    const _id=req.params.id
+    Tasks.findById(_id).then(task=>{
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch(e=>{
+        res.status(500).send(e)
+    })
+})
+
 app.listen(port,()=>{
     console.log("Listening on port "+port)
 
